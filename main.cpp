0dd74afd33e101469113bdebd15a5fae50cdd1f8
biancalj2 <93570691+biancalj2@users.noreply.github.com>
2022-02-25 16:34:12 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include <SparkFunLSM9DS1.h>
 
 /*---------------Pin Numbers----------------*/
-#define SPIN_PIN_DIR 9
-#define SPIN_PIN_EN 10
+#define SPIN_PIN_DIR1 9
+#define SPIN_PIN_DIR2 10
+#define SPIN_PIN_EN 6
 #define ENABLE1 A8//input to EN pin of RIGHT MOTOR
 #define ENABLE2 A9//input to EN pin of LEFT MOTOR
 #define IN1 0//input to IN1
@@ -17,8 +18,13 @@
 #define PIN_DIR 4 //pinDirection for Stepper Motor
 #define LIM1 13//top limit switch
 #define LIM2 20//bottom limit switch
-int echoPin = 11; // Pin 12 for 2nd Sensor attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 16 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define START_SWITCH 7
+#define RED_BLUE_SWTICH 8
+#define TRIG_PIN 16
+int echoPin = 11; // Pin 12 for 2nd Sensor attach pin D2 Arduino to pin Echo of HC-SR04
+#define LEFT_REFLECTOR A0
+#define RIGHT_REFLECTOR A1
 
 /*---------------State Definitions--------------------------*/
 typedef enum {
@@ -29,7 +35,9 @@ typedef enum {
 Robot_states robot_state;
 
 int currentTime;
-int spinDir = 1;
+int team; // bool for red (1) or blue (0)
+int spinDir1 = 0;
+int spinDir2 = 1;
 int spinEnable = 0;
 int dist_th=15;//distance threshold
 int rev=0;
@@ -43,9 +51,13 @@ elapsedMillis UltraSens;
 int trig = false;
 int duration;
 int distance;
+int left_ref;
+int right_ref;
+int black_line; // threshold for reading a black line
 
 /*---------------Function Declarations----------------*/
 void checkGlobalEvents(void);
+void waitToStart(void);
 void spinFlap(void);
 void motorsStraight(int v1, int v2, int speed=255);
 /* values of v1 & v2 determine the direction of rotation of the motors.
@@ -65,6 +77,8 @@ void vector_normalize(float a[3]);
 int get_heading(float acc[3], float mag[3], float p[3]);
 void get_scaled_IMU(float Axyz[3], float Mxyz[3]);
 int final_heading(void);
+void alignWheels(void);
+
 /*---------------IntervalTimer Declarations----------------*/
 IntervalTimer backUpTimer;
 
@@ -102,16 +116,17 @@ float M_Ainv[3][3]
 float declination = -13.6;
 
 float p[] = {0, 1, 0};  //Y marking on sensor board points toward yaw = 0
+
 /*---------------Setup Code----------------*/
 void setup() {
   Serial.begin(9600);
-  
+
   // initialize all states here
   robot_state = WAITING;
-  //wheel_state = STILL;
     
   // initialize pin I/O here
-  pinMode(SPIN_PIN_DIR, OUTPUT);
+  pinMode(SPIN_PIN_DIR1, OUTPUT);
+  pinMode(SPIN_PIN_DIR2, OUTPUT);
   pinMode(SPIN_PIN_EN, OUTPUT);
   pinMode(ENABLE1, OUTPUT);
   pinMode(ENABLE2, OUTPUT);
@@ -123,15 +138,21 @@ void setup() {
   pinMode(LIM2,INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(START_SWITCH, INPUT);
+  pinMode(RED_BLUE_SWTICH, INPUT);
+  pinMode(LEFT_REFLECTOR, INPUT);
+  pinMode(RIGHT_REFLECTOR, INPUT);
     
   // initialize interval timers here
 
   // other setup code here
   currentTime = millis();
-  digitalWrite(SPIN_PIN_DIR, spinDir);
+  digitalWrite(SPIN_PIN_DIR1, spinDir1);
+  digitalWrite(SPIN_PIN_DIR2, spinDir2);
   digitalWrite(SPIN_PIN_EN, spinEnable);
   myStepper1.setMaxSpeed(maxspeed);
   myStepper1.setSpeed(maxspeed);
+  black_line = 600;
 }
 
 /*---------------Main Loop----------------*/
@@ -191,7 +212,10 @@ void loop() {
         }
         break;
     case ALIGN:
-        if (line1() == HIGH && line2() == HIGH){
+        alignWheels();
+        if(right_ref < black_line && left_ref < black_line){
+        //if (line1() == HIGH && line2() == HIGH){
+        //NOTE sensors will not read high/low, they have 0-1023 value. BJ changed this "if" condition to the above line
           if(rev==0){
             motorsStill();
             myStepper1.setPinsInverted(true);
@@ -224,7 +248,19 @@ void loop() {
 /*---------------Function Definitions----------------*/
 void checkGlobalEvents(void){
   // watch for 2min10s timer
+
   // red vs blue team
+  if(digitalRead(RED_BLUE_SWTICH)){
+    team = 1; // red;
+    hdes = 0;
+    // SET OTHER VALUES HERE
+  }
+  else{
+    team = 0; //blue;
+    hdes = 180;
+    // SET OTHER VALUES HERE
+  }
+
   // read potentiometer input for motor speed determination
   if (UltraSens >=2 && trig == false){ // waits 2 milliseconds
     digitalWrite(trigPin,HIGH);
@@ -245,6 +281,17 @@ void checkGlobalEvents(void){
   head = final_heading();
 
 }
+
+void waitToStart(void){
+  if (digitalRead(START_SWITCH)) {
+    start_heading = final_heading() // get current imu reading
+    robot_state = LOADING; // move to loading when switch is flipped
+  }
+  else{
+    robot_state = WAITING; // redundant to ensure we stay in waiting until ready
+  }
+}
+
 void spinFlap(void){
   if (spinEnable == 0) {
     spinEnable = 1;
@@ -299,6 +346,28 @@ void backUp(void){
     b=0;
   }  
 
+}
+
+void alignWheels(void){
+  // go forward when both wheels see white
+  if(right_ref > black_line && left_ref > black_line) {
+    motorsStraight(HIGH, HIGH);
+  }
+
+  // turn left when left reads black
+  if(right_ref > black_line && left_ref < black_line) {
+    motorsTurn(LOW, HIGH);
+  }
+
+  // turn right when right reads black
+  if(right_ref < black_line && left_ref > black_line) {
+    motorsTurn(HIGH, LOW);
+  }
+
+  // stop when both wheels see black
+  if(right_ref < black_line && left_ref < black_line) {
+    motorsStill();
+  }
 }
 
 // basic vector operations
